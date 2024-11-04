39de4cec3dd9a8c6469884bfb07960424ad9dbb1
palll <valzhahyruakbar@gmail.com>
2024-11-04 09:32:19 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'For Sapina'.split('')
+const text = 'For Sapinaa'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
